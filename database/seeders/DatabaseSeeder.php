127579e77203850d7338500676e9c15a82e40f4d
N1k143 <nikitapetrunin296@gmail.com>
2025-11-24 14:01:19 +0500

    add factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Country;
+use App\Models\Gender;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
@@ -15,9 +20,37 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+
+        Gender::factory()->create([ 'name' => 'Male']);
+        Gender::factory()->create([ 'name' => 'Female']);
+
+        Country::factory()->create([ 'name' => 'United States']);
+        Country::factory()->create([ 'name' => 'Russia']);
+        Country::factory()->create([ 'name' => 'USA']);
+        Country::factory()->create([ 'name' => 'China']);
+        Country::factory()->create([ 'name' => 'Kazakhstan']);
+        Country::factory()->create([ 'name' => 'Japan']);
+        Country::factory()->create([ 'name' => 'Germany']);
+
+        Category::factory()->create(['name' => 'Technology']);
+        Category::factory()->create(['name' => 'Horror']);
+        Category::factory()->create(['name' => 'Fashion']);
+        Category::factory()->create(['name' => 'Drama']);
+        Category::factory()->create(['name' => 'Superhero']);
+        Category::factory()->create(['name' => 'Sport']);
+        Category::factory()->create(['name' => 'Thriller']);
+        Category::factory()->create(['name' => 'Comedy']);
+
+
+
+
+
+
+
     }
 }
