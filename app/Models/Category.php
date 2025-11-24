125e97401c346d4f198be0790f4fce3926e928c2
N1k143 <nikitapetrunin296@gmail.com>
2025-11-24 13:23:14 +0500

    first commit

---
+++ app/Models/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory,SoftDeletes;
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'parent_id',
+    ];
+    public function categories(){
+        return $this->hasMany(Category::class);
+    }
+    public function parentCategory(){
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+    public function films(){
+        return $this->belongsToMany(Film::class, 'category_film');
+    }
+}
